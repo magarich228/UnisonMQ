6678c9e40b957bc1265154aaa4687dfd810990cc
magarich228 <kirillgroshev52@gmail.com>
2025-05-08 01:08:28 +0300

    desktop components removed.

--- src/clients/UnisonMQ.UI/App.axaml.cs
+++ src/clients/UnisonMQ.UI/App.axaml.cs
@@ -18,17 +18,7 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
--- src/clients/UnisonMQ.UI/ViewModels/MainViewModel.cs
+++ src/clients/UnisonMQ.UI/ViewModels/MainViewModel.cs
@@ -5,5 +5,5 @@ namespace UnisonMQ.UI.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    private string _greeting = "UnisonMQ";
 }
